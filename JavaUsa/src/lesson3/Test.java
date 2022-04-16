8e6d9305491ba9f094ea76878bd4049793fc60d1
PervinHesenov105 <83514737+PervinHesenov105@users.noreply.github.com>
2022-04-16 18:35:09 +0400

    ilk repom

---
+++ JavaUsa/src/lesson3/Test.java
@@ -0,0 +1,17 @@
+
+package lesson3;
+
+
+public class Test<X> {
+   private X a;
+
+    public X getA() {
+        return a;
+    }
+
+    public void setA(X a) {
+        this.a = a;
+    }
+   
+   
+}
